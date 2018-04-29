42259c0cc52c1178a17b1b951238bac09e44506d
Robp773 <robpeterman573@gmail.com>
2018-04-28 23:28:02 -0400

    broke 'filter' service into three seperate services + moved graph data scope values to rootscope to enable future filtering

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ angular.module('root-app', ['chart.js'])
     return data;
   })
 
-  .service('filter', function () {
-    this.standardDeviation = function(values){         
+  .service('stdDeviation', function () {
+    this.sqrDiff = function(values){         
       let stdDevArray = [];
       for(let value in values){      
         stdDevArray.push(values[value]);
@@ -35,31 +35,50 @@ angular.module('root-app', ['chart.js'])
       let avg = sum / data.length;
       return avg;   
     };
+  })
 
-    this.setGraphData = function(result, metric){
-      let labelOrder=  [];
+  .service('setGraphs', function (stdDeviation) {
+
+    this.lineGraphData = function(result){
+      let labelOrder =  [];
       let dataArray = [];
       let stdDevArray = [];
-      if(metric === 'lineValueLost'){
-        let totalMonthlyLoss = 0;
-        for(let airline in result){
-          totalMonthlyLoss += result[airline].valueLost;
-          dataArray.push(Math.round(result[airline].valueLost / 12));
-          labelOrder.push(airline);
-        }
-        return {labelOrder: labelOrder, dataArray: dataArray, totalLossesAvg: Math.round(totalMonthlyLoss / 12)};              
-      }       
-      else{
-        for(let airport in result){
-          labelOrder.push(airport);
-          dataArray.push(Math.round(result[airport].totalClaims / 12));
-          stdDevArray.push(this.standardDeviation(result[airport].monthlyClaims));
-        }        
+      let statsObj = {};
+  
+      let totalMonthlyLoss = 0;
+      for(let airline in result){
+        statsObj[airline] = result[airline];
+        totalMonthlyLoss += result[airline].valueLost;
+        dataArray.push(Math.round(result[airline].valueLost / 12));
+        labelOrder.push(airline);
       }
-      return {labelOrder, dataArray, stdDevArray};
-    };
+      let claimKeys = ['approved', 'denied', 'settled'];
+      for(let airline in statsObj){
+        for(let i=0; i<claimKeys.length; i++){
+          statsObj[airline].claimStatus[claimKeys[i]] =  ((statsObj[airline].claimStatus[claimKeys[i]] / statsObj[airline].claimStatus.totalClaims) * 100).toFixed(1) + '%';
+        }
+      }
+      return {statsObj: statsObj, labelOrder: labelOrder, dataArray: dataArray, totalLossesAvg: Math.round(totalMonthlyLoss / 12)};              
+    };  
 
-    this.lineGraph = function (data, metric, airlineFilter) {
+    this.barGraphData = function(result){
+      let labelOrder =  [];
+      let dataArray = [];
+      let stdDevArray = [];
+      // let statsObj = {};
+  
+      for(let airport in result){
+        labelOrder.push(airport);
+        dataArray.push(Math.round(result[airport].totalClaims / 12));
+        stdDevArray.push(stdDeviation.sqrDiff(result[airport].monthlyClaims));
+      }    
+      return {labelOrder, dataArray, stdDevArray, };
+    };
+  })
+
+  .service('filter', function (setGraphs) {
+
+    this.lineGraph = function (data, airlineFilter) {
       // object to keep totals and numbers for additional statistics
       let result = {};        
       // loop through the data from dataset.json
@@ -70,24 +89,34 @@ angular.module('root-app', ['chart.js'])
           if(data[i]['Airline Name'] === airlineFilter[x]){
             // and the airport name already has been initialized in the result object
             if(data[i]['Airline Name'] in result){
-            // monthNum gets the incident date month number from the current index 
+              if(data[i].Disposition === 'Approve in Full'){
+                result[data[i]['Airline Name']].claimStatus.approved++;
+              }
+              else if(data[i].Disposition === 'Deny'){
+                result[data[i]['Airline Name']].claimStatus.denied++;
+              }
+              else if(data[i].Disposition === 'Settle'){
+                result[data[i]['Airline Name']].claimStatus.settled++;
+              }
+              // monthNum gets the incident date month number from the current index
+              let monthNum = data[i]['Incident Date'].substring(0, 2).replace( /\D+/g, '');
+              result[data[i]['Airline Name']].monthlyClaims[monthNum]++;
               // increment this airports totalClaims number to track total claims
-              result[data[i]['Airline Name']].totalClaims++;
+              result[data[i]['Airline Name']].claimStatus.totalClaims++;
               // adds up each airport's value lost and updates the result object as it goes
-              result[data[i]['Airline Name']].valueLost = result[data[i]['Airline Name']].valueLost + Math.round(Number(data[i]['Close Amount'].replace(/[^0-9\.]+/g,'')));
-              // keeps track of the claims per month by airline by incrementing 
+              result[data[i]['Airline Name']].valueLost = result[data[i]['Airline Name']].valueLost + Number(data[i]['Close Amount'].replace(/[^0-9\.]+/g,''));
             }
             // if airport name has not been initialized in the result object, add it in with default values
             else{
-              result[data[i]['Airline Name']] = {totalClaims: 0, valueLost: 0};
+              result[data[i]['Airline Name']] = {claimStatus:{totalClaims: 0, approved: 0, denied:0, settled: 0}, valueLost: 0, monthlyClaims: {1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0, 10:0, 11:0, 12:0}};
             }
           }
         }
       }
-      return this.setGraphData(result, metric);
+      return setGraphs.lineGraphData(result);
     };
     
-    this.barGraph = function(data, metric, airportFilter){
+    this.barGraph = function(data, airportFilter){
       let resultsObj = {};
       for(let i=0;i<data.length; i++){
         for(let x=0; x<airportFilter.length; x++){
@@ -104,24 +133,25 @@ angular.module('root-app', ['chart.js'])
           }
         }
       }
-      return this.setGraphData(resultsObj, metric);
+      return setGraphs.barGraphData(resultsObj);
     };
   })
 
   .controller('LineCtrl', function ($scope, $rootScope, filter, getData) {  
     getData.then(function(res){ 
-      $rootScope.airlineFilter = ['American Airlines', 'Delta Air Lines', 'Lufthansa', 'Alaska Airlines'];
-      $scope.lineGraphData = filter.lineGraph(res, 'lineValueLost', $rootScope.airlineFilter);
-      $scope.data = $scope.lineGraphData.dataArray;
-      $scope.labels = $scope.lineGraphData.labelOrder;
-      $scope.totalLossesAvg = $scope.lineGraphData.totalLossesAvg;
+      $rootScope.airlineFilter = ['American Airlines', 'Delta Air Lines', 'Lufthansa', 'Alaska Airlines', 'Allegiant Air', 'Jet Blue'];
+      let lineGraphData = filter.lineGraph(res, $rootScope.airlineFilter);
+      $rootScope.lineData = lineGraphData.dataArray;
+      $rootScope.lineLabels = lineGraphData.labelOrder;
+      $rootScope.totalLossesAvg = lineGraphData.totalLossesAvg;
+      $rootScope.lineGraphStats = lineGraphData.statsObj;
     });
   })
 
   .controller('BarCtrl', function ($scope, $rootScope, filter, getData) { 
     getData.then(function(res){    
       $rootScope.airportFilter = ['CVG','DEN','LAX', 'ORD', 'SEA']; 
-      $scope.barGraphData = filter.barGraph(res, 'barAvgClaims', $rootScope.airportFilter);
+      $scope.barGraphData = filter.barGraph(res, $rootScope.airportFilter);
       $scope.data = $scope.barGraphData.dataArray;
       $scope.labels = $scope.barGraphData.labelOrder;
       let standardDeviationArrary = $scope.barGraphData.stdDevArray;
@@ -138,9 +168,20 @@ angular.module('root-app', ['chart.js'])
     });        
   })
 
-  .controller('ToggleCtrl', function($scope) {
+  .controller('FilterToggleCtrl', function($scope, $rootScope, filter) {
     $scope.metric = 'lineValueLost';
     $scope.toggle = function(toggle){
       $scope.metric = toggle; 
+    };
+
+    $scope.filterAirline = function(airline){
+      // console.log(airline)
+      for(let i =0; i<$rootScope.airlineFilter.length; i++){
+        if($rootScope.airlineFilter[i] === airline){
+          $rootScope.airlineFilter.splice(i, 1);
+          filter.line;
+          return;
+        }
+      }
     };
   });  
